929540a666e17eec316e3bcdb4b2230bd49c44d7
duanhaoling <748964501@qq.com>
2016-03-22 18:42:01 +0800

    新增数据库帮助类,以及各表对应的实体类.

---
+++ src/main/java/com/example/coolweather/model/Province.java
@@ -0,0 +1,34 @@
+package com.example.coolweather.model;
+
+/**
+ * Created by duanhaoling on 2016/3/22.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
